52ee6c1c58616e216c6310dd092d61120610b71e
sarav <saravrajavelu@gmail.com>
2014-12-30 01:52:40 +0530

    Takes user's specifications

--- temporaryRun.py
+++ temporaryRun.py
@@ -1,19 +1,7 @@
-import re
-import requests
-import sqlite3
-import nltk
-import time
-from bs4 import BeautifulSoup
-url = 'http://www.thesaurus.com/browse/' + 'bad' + '?s=t'
-req = requests.get(url)
-soup = BeautifulSoup(req.content)
-temp = soup.find_all("div",{"class":"heading-block oneClick-disabled"})
-no_of_syn = re.findall(r'<strong>(.*)</strong>',str(temp[0]))
-no_of_syn = int(no_of_syn[0])
-relevant_list = soup.find_all("span",{"class":"text"},limit = no_of_syn)
-for list_item in relevant_list:
-    
-    print(list_item)
-    
-
-print(no_of_syn)
+no_spec = int(input('Enter no.of specifications : '))
+print (' Choose specifications : ')
+temp_count = 0
+specificationList = ['']*no_spec
+while temp_count < no_spec:
+    specificationList[temp_count] = input()
+    temp_count += 1
