f1d52c419366eacf00c56e59c518970086564ab5
saravrajavelu <saravrajavelu@gmail.com>
2015-02-21 11:57:03 +0530

    fixed productReview class

--- class.py
+++ class.py
@@ -1,19 +1,19 @@
 class productReview:
-	def __init__():
-		userReviews = []
-		userScores = []
-		length = 0
-	def addReview(review,score):
-		userReviews.append(str(review))
-		userScores.append(str(review))
-		length += 1
-	def displayList(limit=-1):
+	def __init__(self):
+		self.userReviews = []
+		self.userScores = []
+		self.length = 0
+	def addReview(self,review,score):
+		self.userReviews.append(str(review))
+		self.userScores.append(str(review))
+		self.length += 1
+	def displayList(self,limit=-1):
 		if limit == -1:
-			span = length
+			span = self.length
 		else:
 			span = limit
 		for itemno in range(span):
 			print('----------------------------------------------------')
-			print('User Score : ',userScores[itemno])
+			print('User Score : ',self.userScores[itemno])
 			print('User Review : ')
-			print(userReviews[itemno])
+			print(self.userReviews[itemno])
