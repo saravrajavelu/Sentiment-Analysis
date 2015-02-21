cd547b7a6cf21fd14c0264c02bcbfde036743fa4
saravrajavelu <saravrajavelu@gmail.com>
2015-02-21 16:45:07 +0530

    productReview class fixed

--- pullingReviews_Final.py
+++ pullingReviews_Final.py
@@ -2,8 +2,28 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
+class productReview:
+	def __init__(self):
+		self.userReviews = []
+		self.userScores = []
+		self.length = 0
+	def addReview(self,review,score):
+		self.userReviews.append(str(review))
+		self.userScores.append(str(score))
+		self.length += 1
+	def displayList(self,limit=-1):
+		if limit == -1:
+			span = self.length
+		else:
+			span = limit
+		for itemno in range(span):
+			print('----------------------------------------------------')
+			print('User Score : ',self.userScores[itemno])
+			print('User Review : ')
+			print(self.userReviews[itemno])
 
 
+reveiwList = productReview()
 
 url = 'http://www.flipkart.com/search'
 MAIN_SITE = 'http://www.flipkart.com'
@@ -40,8 +60,25 @@ review_url = re.sub(r'&otracker.+','&type=all',temp_url)
 
 req_url = requests.get(review_url)
 soup = BeautifulSoup(req_url.content)
+nom = 0
 
+'''
+fullReview = soup.find_all(attrs={"class":"fclear fk-review fk-position-relative line "})
+for item in fullReview:
+    m = re.search(r'<span class=\"review-text\">(.*) </span>',str(item),re.DOTALL)
+    tempSoup = BeautifulSoup(m.group(0))
+    sco = re.findall(r'<div class=\"fk-stars\" title=\"(\d) stars\">',str(item))
+    reveiwList.addReview(tempSoup.text,sco[0])
+    nom += 1
+    print(nom)
+'''
+'''
 review_list = soup.find_all("span",{"class":"review-text"})
+userScore = soup.find_all(attrs={"class":"fk-stars"})
+for item in userScore:
+    print(re.findall(r'<div class=\"fk-stars\" title=\"(\d) stars\">',str(item)))
+'''
+
 
 review_statement = soup.find_all("span",{"class":"nav_bar_result_count"})
 review_statement = str(review_statement[0])
@@ -53,7 +90,7 @@ reviews_per_page = int(list_review_info[1])
 
 temp_url = re.sub(r'type=all','rating=1,2,3,4,5&reviewers=all&type=all&sort=most_helpful&start=',review_url)
 
-for i in range(10,(total_no_of_reviews + 1),10):
+for i in range(0,(total_no_of_reviews + 1),10):
     if i > total_no_of_reviews:
         break
     else:
@@ -61,9 +98,24 @@ for i in range(10,(total_no_of_reviews + 1),10):
         
         req_temp_url = requests.get(temp_url+section_number)
         soup_temp = BeautifulSoup(req_temp_url.content)
-        review_list = review_list + soup_temp.find_all("span",{"class":"review-text"})
+        #review_list = review_list + soup_temp.find_all("span",{"class":"review-text"})
 
-print('No.of reviews found : ' + str(len(review_list)))
+
+        fullReview = soup_temp.find_all(attrs={"class":"fclear fk-review fk-position-relative line "})
+        for item in fullReview:
+            m = re.search(r'<span class=\"review-text\">(.*) </span>',str(item),re.DOTALL)
+            tempSoup = BeautifulSoup(m.group(0))
+            sco = re.findall(r'<div class=\"fk-stars\" title=\"(\d) stars?\">',str(item))
+            reveiwList.addReview(tempSoup.text,sco[0])
+            nom += 1
+            print(nom)
+
+
+
+
+        
+
+print('No.of reviews found : ' + str(reveiwList.length))
 
 
 ''' Remove this comment to print all the reviews
