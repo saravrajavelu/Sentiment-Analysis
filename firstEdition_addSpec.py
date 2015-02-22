a22e6c34a7ee6bd43f79b348a4dd6a0b4bd9d05c
saravrajavelu <saravrajavelu@gmail.com>
2015-02-22 13:36:31 +0530

    Socket error generated, but completed productReview class

--- firstEdition_addSpec.py
+++ firstEdition_addSpec.py
@@ -6,6 +6,62 @@ import nltk
 import time
 from bs4 import BeautifulSoup
 
+#Declare productReview class
+class productReview:
+    def __init__(self):
+        self.userReviews = []
+        self.userScores = []
+        self.productFeatures = []
+        self.featureScore = []
+        self.featureOccurence = []
+        self.count = 0
+
+    def addReview(self,review,score):
+        self.userReviews.append(str(review))
+        self.userScores.append(str(score))
+        self.count += 1
+
+    def displayList(self,limit=-1):
+        if limit == -1:
+            span = self.count
+        else:
+            span = limit
+        for itemno in range(span):
+            print('----------------------------------------------------')
+            print('User Score : ', self.userScores[itemno])
+            print('User Review : ')
+            print(self.userReviews[itemno])
+
+    def updateFeature(self,feature,score):
+        index = self.productFeatures.index(feature)
+        self.featureScore[index] += score
+        self.featureOccurence[index] += 1
+
+    def addFeatures(self):
+            noSpec = int(input('Enter no.of specifications : '))
+            print (' Choose specifications : ')
+            for i in range(noSpec):
+                feature = input()
+                self.productFeatures.append(str(feature))
+                self.featureOccurence.append(0)
+                self.featureScore.append(0)
+
+    def displayFeature(self):
+            print('***************  Product Summary ***************')
+            for item in self.productFeatures:
+                index = self.productFeatures.index(item)
+                if self.featureOccurence[index] == 0:
+                    print(str(item),' : -NA- ')
+                else:
+                    totalScore = int(self.featureScore[index])/int(self.featureOccurence[index])
+                    print(str(item),' : ',str(totalScore))
+
+
+#Initialize productReview class
+reveiwList = productReview()
+
+
+
 #Connecting to knowledgeBase
 conn = sqlite3.connect('knowledgeBase.db')
 c = conn.cursor()
@@ -18,13 +74,13 @@ MAIN_SITE = 'http://www.flipkart.com'
 
 #Feeding Product Name and passing it to website's search
 QUERY = input('Product Name : ')
-r = requests.get(url)
-payload = {'q':QUERY,'as':'off','as-show':'off','otracker':'start'}
-r = requests.get(url,params=payload)
-#print(r.url)
+#r = requests.get(url)
+payload = {'q': QUERY, 'as' : 'off' , 'as-show' : 'off' , 'otracker' : 'start'}
+r = requests.get(url , params=payload)
 
 
 
+#Getting product listings
 soup =  BeautifulSoup(r.content)
 item_list = soup.find_all("a",{"class":"fk-display-block"})
 count = 0
@@ -33,9 +89,9 @@ for item in item_list:
     print(str(count)+". "+item.get('title'))
 
 
+#Choice product from listing
 choice = input('Choose product : ')
 item = item_list[int(choice)-1]
-#print(MAIN_SITE + item.get('href'))
 
 item_url = MAIN_SITE + item.get('href')
 
@@ -65,12 +121,7 @@ review_url = re.sub(r'&otracker.+','&type=all',temp_url)
 req_url = requests.get(review_url)
 soup = BeautifulSoup(req_url.content)
 
-review_list = soup.find_all("span",{"class":"review-text"})
-'''
-reviews = soup.find_all("span",{"class":"review-text"})
-for review in reviews:
-    review_list.append(str(review.text))
-'''
+
 review_statement = soup.find_all("span",{"class":"nav_bar_result_count"})
 review_statement = str(review_statement[0])
 reg_number = re.compile(r'\d+')
@@ -81,30 +132,28 @@ reviews_per_page = int(list_review_info[1])
 
 temp_url = re.sub(r'type=all','rating=1,2,3,4,5&reviewers=all&type=all&sort=most_helpful&start=',review_url)
 
-for i in range(10,(total_no_of_reviews + 1),10):
+for i in range(0,(total_no_of_reviews + 1),10):
     if i > total_no_of_reviews:
         break
     else:
         section_number = str(i)
-        
+
         req_temp_url = requests.get(temp_url+section_number)
         soup_temp = BeautifulSoup(req_temp_url.content)
-        review_list = review_list + soup_temp.find_all("span",{"class":"review-text"})
-        '''
-        reviews = soup.find_all("span",{"class":"review-text"})
-        for review in reviews:
-            review_list.append(str(review.text))
-        '''
-print('No.of reviews found : ' + str(len(review_list)))
+
+
+        fullReview = soup_temp.find_all(attrs={"class":"fclear fk-review fk-position-relative line "})
+        for item in fullReview:
+            m = re.search(r'<span class=\"review-text\">(.*) </span>',str(item),re.DOTALL)
+            tempSoup = BeautifulSoup(m.group(0))
+            sco = re.findall(r'<div class=\"fk-stars\" title=\"(\d) stars?\">',str(item))
+            reveiwList.addReview(tempSoup.text,sco[0])
 
 
 
-#print (review_list[1].text)
+print('No.of reviews found : ' + str(reveiwList.count))
 
 '''
-specificationList =['Battery','Touch','Display','Budget','Price','Processor','Screen']
-'''
-
 no_spec = int(input('Enter no.of specifications : '))
 print (' Choose specifications : ')
 temp_count = 0
@@ -112,17 +161,15 @@ specificationList = ['']*no_spec
 while temp_count < no_spec:
     specificationList[temp_count] = input()
     temp_count += 1
+'''
 
 
 
-
-def processor(review_list,specificationList):
+def processor(productReview):
     try:
-        rating_for_spec_list = [0]*len(specificationList)
-        hits_for_spec_list = [0]*len(specificationList)
-        for exampleReview in review_list:
+        for exampleReview in productReview.userReviews:
             try:
-                
+
                 sentences = re.findall(r'(.*?)[\.|\?|!+]',exampleReview.text)
                 '''sentences = re.findall(r'(.*?)[\.|\?|!+]',exampleReview)'''
                 for sent in sentences:
@@ -130,7 +177,7 @@ def processor(review_list,specificationList):
                     tagged = nltk.pos_tag(tokenized)
                     namedEnt = nltk.ne_chunk(tagged, binary=True)
                     entities = re.findall(r'NE\s(.*?)/',str(namedEnt))
-                        
+
                     descriptives_noun = re.findall(r'\(\'(\w*)\',\s\'NN\w?\'',str(tagged))
                     descriptives_verbs = re.findall(r'\(\'(\w*)\',\s\'VB\w?\'',str(tagged))
                     descriptives_adj = re.findall(r'\(\'(\w*)\',\s\'JJ\w?\'',str(tagged))
@@ -141,16 +188,15 @@ def processor(review_list,specificationList):
                     adj_list =  descriptives_adverb + descriptives_adj + descriptives_verbs
                     for attr in attr_list:
                         attr = attr.lower().capitalize()
-                        if attr in specificationList:
+                        if attr in productReview.productFeatures:
                              for adj_item  in adj_list:
                                  c.execute('SELECT value from wordVals where word=?',(adj_item.lower(),))
-                                 query_result = c.fetchone()   
+                                 query_result = c.fetchone()
                                  if query_result is None:
                                      pass
                                  else:
-                                     rating_for_spec_list[specificationList.index(attr)] += int(query_result[0])
-                                     hits_for_spec_list[specificationList.index(attr)] += 1
-                            
+                                     productReview.updateFeature(attr , int(query_result[0]))
+
                         else:
                             pass
 
@@ -158,29 +204,10 @@ def processor(review_list,specificationList):
                 print(str(e))
                 print('Error - Processor - 2nd try')
 
+        productReview.displayFeature()
 
-        for spec in specificationList:
-            if hits_for_spec_list[specificationList.index(spec)] == 0:
-                print (spec + ' : NA ')
-            else:
-                temp_rating = rating_for_spec_list[specificationList.index(spec)]/hits_for_spec_list[specificationList.index(spec)]
-                print (spec + ' :  ',temp_rating)
-                
     except Exception as e:
         print(str(e))
         print('Error - Processor - 1st Try')
 
-'''
-def printRating():
-    try:
-        for spec in spec_list:
-            if hits_for_spec_list[spec_list.index(spec)] == 0:
-                print (spec + ' : NA ')
-            else:
-                temp_rating = rating_for_spec_list[spec_list.index(spec)]/hits_for_spec_list[spec_list.index(spec)]
-                print (spec + ' :  ',temp_rating)
-
-'''
-
-
-processor(review_list,specificationList)
+processor(reveiwList)
