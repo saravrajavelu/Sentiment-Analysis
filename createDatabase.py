0344b8b32adaa52e5afd9103e799a6262974e0d3
saravrajavelu <saravrajavelu@gmail.com>
2015-02-23 17:25:26 +0530

    Project Restructuring - Clearing deleted files

--- createDatabase.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('knowledgeBase.db')
-c = conn.cursor()
-
-
-def createDB():
-    c.execute('CREATE TABLE doneSyns (word TEXT, value REAL)')
--- temporaryRun.py
+++
@@ -1,7 +0,0 @@
-no_spec = int(input('Enter no.of specifications : '))
-print (' Choose specifications : ')
-temp_count = 0
-specificationList = ['']*no_spec
-while temp_count < no_spec:
-    specificationList[temp_count] = input()
-    temp_count += 1
